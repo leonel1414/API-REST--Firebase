f977950bc3053692fb6297032ecc0c032b0be112
leonel1414 <leoo9211@hotmail.com>
2025-10-15 21:04:30 -0300

    agregando middleware

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import notFound from './src/middlewares/not-found.js';
 const app = express();
 
 
+app.use((req, res, next) =>{
+    //res.json({ message: "Hola API en mantenemiento"});
+    console.log(req.method);
+    next();
+});
+
+app.use(notFound);
+
 app.get("/", (req,res) =>{
-    res.send("Bienvenidos a nuestra API REST");
+    //res.send("Bienvenidos a nuestra API REST");
+    res.json({ message: "Bienvenidos a nuestra API REST!!"});
 });
 
 const PORT = 3000;
