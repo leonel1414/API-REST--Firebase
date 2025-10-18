491bd809ffb07d3b6e05c6a21faf89f0c995579c
leonel1414 <leoo9211@hotmail.com>
2025-10-18 19:41:48 -0300

    array

--- index.js
+++ index.js
@@ -2,13 +2,49 @@ import express from 'express';
 import notFound from './src/middlewares/not-found.js';
 const app = express();
 
+//Array de productos;
 
+const product =[
+    {
+        id:1,
+        name:"Camisetas Deportiva",
+        price: 150,
+        categorias: ["ropa","deportes"],
+    },
+    {
+        id:2,
+        name:"Zapatos Running",
+        price: 1200,
+        categorias: ["calzados","deportes"],
+    },
+    {
+        id:3,
+        name:"Mochila Escolar",
+        price: 1500,
+        categorias: ["mochila","escolar"],
+    },
+        {
+        id:4,
+        name:"Auriculares Bluetooth",
+        price: 800,
+        categorias: ["tecnología","audio"],
+    },
+    {
+        id:5,
+        name:"Botella Térmica",
+        price: 220,
+        categorias: ["hogar","accesorios"],
+    },
+]
+
+
+/*
 app.use((req, res, next) =>{
     //res.json({ message: "Hola API en mantenemiento"});
     console.log(req.method);
     next();
 });
-
+*/
 app.use(notFound);
 
 app.get("/", (req,res) =>{
